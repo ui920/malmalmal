ef7c5da3d52a699b3d040f653f82fbaf167c4619
UI920 <180168040+ui920@users.noreply.github.com>
2025-04-17 23:10:14 +0900

    플리부분 빼고 완료

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function showPopups() {
+  alert(
+    '어떤 경로로 이곳까지 오셨는지 모르겠지만...\nWe’re not sure how you ended up here...'
+  );
+  alert(
+    '어쨌든 말이 넘치고 흐르고 정리도 잘 안돼는 공간에 오신 걸 환영합니다!\nWelcome to this space overflowing with messy, tangled words.'
+  );
+  alert(
+    '여긴 말이 많아서 아무 데나 눌러도 괜찮아요. 이리저리 마구 눌러보세요!\nThere’s so much talk going on, so feel free to click anywhere. Explore, wander, and tap around as much as you like!'
+  );
+  window.location.href = 'page.html';
+}
