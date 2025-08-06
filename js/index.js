ae06637daa263f6820677f1cd70fbb73cb56ba22
UI920 <180168040+ui920@users.noreply.github.com>
2025-08-06 15:33:38 +0900

    수돗꼭지 소리 추가

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function showPopups() {
     '어쨌든 말이 넘치고 흐르고 정리도 잘 안돼는 공간에 오신 걸 환영합니다!\nWelcome to this space overflowing with messy, tangled words.'
   );
   alert(
-    '여긴 말이 많아서 아무 데나 눌러도 괜찮아요. 이리저리 마구 눌러보세요!\nThere’s so much talk going on, so feel free to click anywhere. Explore, wander, and tap around as much as you like!'
+    '여긴 말이 많아 여기저기 눌러보세요. 어쩌면 소리가 튀어나올지도 몰라요!\nThere’s a lot of chatter here. Try clicking around — a sound might just pop out!'
   );
   window.location.href = 'page.html';
 }
